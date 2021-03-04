c68504098ef9c546029339c8a87b198d78923300
miles-mcgibbon <s1738716@ed.ac.uk>
2021-03-04 21:27:02 +0000

    Resolved #1: Peptides ignored and script validates active ligand

--- split_MOAD_complex.py
+++ split_MOAD_complex.py
@@ -9,13 +9,12 @@ filterwarnings('ignore')
 
 class ligand_selector(Select):
 
-    def __init__(self, residue, id_code):
-        self.residue = residue
-        self.id_code = id_code
+    def __init__(self, id_codes):
+        self.id_codes = id_codes
 
     def accept_residue(self, residue):
         # select residues from identified ligand
-        return True if residue.id[0] == self.id_code else False
+        return True if residue.id[0].replace('H_','') in self.id_codes else False
 
 class protein_selector(Select):
 
@@ -27,81 +26,58 @@ def split_structure(filename, destination_path, datafile, make_dir, ignore_pepti
 
     global ligand_errors
 
-    # list of hetatm ids to ignore
-    ignore_list = ['SO4','HOH']
-
     # define pdb code
     pdb_code = filename.split('.')[0].split('/')[filename.count('/')]
+    ext = filename.split('.')[1].replace('bio','')
 
     # load the pdb structure
     parser = PDBParser()
     structure = parser.get_structure(pdb_code, filename)
 
+    pdb_code_data = datafile.loc[datafile.PDBCode.str.upper() == pdb_code.upper()]
+    ligand_data = pdb_code_data.loc[pdb_code_data.Validity == 'valid']
+    valid_ligand_ids = [ligand.split(':')[0].split(' ') for ligand in list(ligand_data.LigandID)]
 
-    # find all the het residues/non amino acids
-    residues = list(structure.get_residues())
-    ligands = [res for res in residues if 'H_' in res.id[0]]
+    # set the structure for saving
+    io = PDBIO()
+    io.set_structure(structure)
 
-    # get the number of atoms all the ligands and store them in a dictionary
-    ligand_atoms = {}
-    for lig in ligands:
-        ligand_atoms[len(lig.child_dict.values())] = lig.id
-
-    try:
-        # get the longest ligands, excluding water and metal ions
-        active_ligand_ids = [ligand_atoms[key][0] for key in ligand_atoms.keys() if key > 3 and ligand_atoms[key][0].replace('H_','') not in ignore_list]
-        print(active_ligand_ids)
-
-        # set the structure for saving
-        io = PDBIO()
-        io.set_structure(structure)
-
-        if make_dir:
-            try:
-                os.mkdir(f'{destination_path}{pdb_code}')
-            except FileExistsError:
-                pass
-
-            # save the ligands
-            for residue in structure.get_residues():
-                if residue.id[0] in active_ligand_ids:
-                    io.save(f"{destination_path}{pdb_code}/{pdb_code}_ligand_{residue.id[0]}.pdb", ligand_selector(residue, residue.id[0]))
-
-        # save the protein
-        io.save(f"{destination_path}{pdb_code}/{pdb_code}_protein.pdb", protein_selector())
-        input('')
-
-    except ValueError:
-
-        if ignore_peptide:
+    if make_dir:
+        try:
+            os.mkdir(f'{destination_path}{pdb_code}')
+        except FileExistsError:
+            new_pdb_code = pdb_code + f'_{ext}'
+            os.mkdir(f'{destination_path}{new_pdb_code}')
             pass
-        else:
-            # add flag to directory if no ligand found
 
-            # set the structure for saving
-            io = PDBIO()
-            io.set_structure(structure)
+    for id_codes in valid_ligand_ids:
+        io.save(f"{destination_path}{pdb_code}/{pdb_code}_ligand.pdb", ligand_selector(id_codes))
 
-            if make_dir:
-                try:
-                    os.mkdir(f'{destination_path}{pdb_code}_LIGAND_ERROR')
-                except FileExistsError:
-                    pass
-
-            # save the protein
-            io.save(f"{destination_path}{pdb_code}_LIGAND_ERROR/{pdb_code}_protein.pdb", protein_selector())
-
-        ligand_errors = ligand_errors + 1
+    io.save(f"{destination_path}{pdb_code}/{pdb_code}_protein.pdb", protein_selector())
 
 complex_path = '/home/milesm/Dissertation/Data/Raw/Binding_MOAD/Extracted/BindingMOAD_2020/'
 destination_path = '/home/milesm/Dissertation/Data/Parsed/Binding_MOAD/'
 binding_MOAD_datafile_path = '/home/milesm/Dissertation/Data/Raw/Binding_MOAD/Compressed/nr_bind.csv'
 
+force_columns = ['Decimal',
+                 'EntryString',
+                 'PDBCode',
+                 'LigandID',
+                 'Validity',
+                 'BindingDataType',
+                 'Equals',
+                 'BindingValue',
+                 'BindingUnits',
+                 'FormulaString',
+                 'FormulaStringSpill']
+
 complexes = os.listdir(complex_path)
 
 ligand_errors = 0
 
-binding_df = pd.read_csv(binding_MOAD_datafile_path)
+binding_df = pd.read_csv(binding_MOAD_datafile_path, names=force_columns)
+
+binding_df['PDBCode'] = binding_df['PDBCode'].ffill()
 
 with tqdm(total=len(complexes)) as pbar:
     for complex in complexes:
