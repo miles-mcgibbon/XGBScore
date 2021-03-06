009f57dbf800ae0a02220bf30c07a65727e9b84b
miles-mcgibbon <s1738716@ed.ac.uk>
2021-03-06 22:29:56 +0000

    Converter ammended to deal with full redundant set

--- pdbqt_batch_converter.py
+++ pdbqt_batch_converter.py
@@ -3,25 +3,16 @@ import oddt
 import subprocess
 from tqdm import tqdm
 
-MOAD_fatal_error_list = list()
-PDB_fatal_error_list = list()
+fatal_error_list = list()
 
-pdb_files_path = '/home/milesm/Dissertation/Data/Parsed/Non_redundant/PDBBind/'
-moad_files_path = '/home/milesm/Dissertation/Data/Parsed/Non_redundant/Binding_MOAD/'
-dude_files_path = ''
+druglike_files_path = '/home/milesm/Dissertation/Data/Parsed/Non_redundant/Full_Druglike/'
 
 prep_ligand_command = '/home/milesm/Dissertation/Third_Party_Code/MGLTools-1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_ligand4.py'
 prep_protein_command = '/home/milesm/Dissertation/Third_Party_Code/MGLTools-1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_receptor4.py'
 
-destination_path = '/home/milesm/Dissertation/Data/PDBQT/Non_redundant/'
+destination_path = '/home/milesm/Dissertation/Data/PDBQT/Non_redundant_druglike/'
 
-example_ligand_file = '/home/milesm/Desktop/4yki_ligand.pdb'
-example_protein_file = '/home/milesm/Desktop/7cpa/7cpa_protein.pdb'
-example_destination_path = '/home/milesm/Desktop/7cpa/'
-
-pdb_structure_folders = [(pdb_files_path + folder) for folder in os.listdir(pdb_files_path)]
-moad_structure_folders = [(moad_files_path + folder) for folder in os.listdir(moad_files_path)]
-#dude_structure_folders = [(dude_files_path + folder) for folder in os.listdir(dude_files_path)]
+druglike_structure_folders = [(druglike_files_path + folder) for folder in os.listdir(druglike_files_path)]
 
 def repair_stacked_ligand(ligand_filepath, ligand_file):
     ligand_raw_text = open(ligand_filepath, 'r').read()
@@ -47,13 +38,13 @@ def repair_stacked_ligand(ligand_filepath, ligand_file):
             print(f'Found pre-repaired ligand from a previous run: "{repair_check[0]}"')
         return repaired_ligand_filepath
 
-def autodock_convert(folder_name, destination_path, prep_ligand_command, prep_protein_command, ligand_filepath, ligand_file, receptor_filepath, receptor_file, database_name):
+def autodock_convert(folder_name, destination_path, prep_ligand_command, prep_protein_command, ligand_filepath, ligand_file, receptor_filepath, receptor_file, filetype):
     try:
         os.mkdir(f'{destination_path}{folder_name}')
     except FileExistsError:
         pass
     print(folder_name)
-    if database_name == 'pdb':
+    if filetype == 'mol2':
         ligand_outfile = ligand_file.replace('mol2','pdb')
     else:
         ligand_outfile = ligand_file
@@ -68,75 +59,41 @@ def autodock_convert(folder_name, destination_path, prep_ligand_command, prep_pr
                 output = subprocess.check_output(f'{prep_ligand_command} -l {repaired_ligand_filepath} -A hydrogens -o {destination_path}{folder_name}/{ligand_outfile}qt', shell=True)
                 print('REPAIR SUCCESSFUL!')
         except:
-            if database_name == 'moad':
-                MOAD_fatal_error_list.append(folder_name)
-                if os.path.isfile('MOAD_fatal_error_list.txt'):
-                    os.remove('MOAD_fatal_error_list.txt')
-                with open('MOAD_fatal_error_list.txt','a+') as error_list:
-                    error_list.write(str(MOAD_fatal_error_list))
-                    error_list.close()
-            if database_name == 'pdb':
-                PDB_fatal_error_list.append(folder_name)
-                if os.path.isfile('PDB_fatal_error_list.txt'):
-                    os.remove('PDB_fatal_error_list.txt')
-                with open('PDB_fatal_error_list.txt','a+') as error_list:
-                    error_list.write(str(PDB_fatal_error_list))
-                    error_list.close()
+            fatal_error_list.append(folder_name)
+            if os.path.isfile('fatal_error_list.txt'):
+                os.remove('fatal_error_list.txt')
+            with open('fatal_error_list.txt','a+') as error_list:
+                error_list.write(str(fatal_error_list))
+                error_list.close()
             print(f'FATAL PROBLEM WITH {folder_name}: Added to list and skipping...')
 
     print('Preparing protein...')
     try:
         output = subprocess.check_output(f'{prep_protein_command} -r {receptor_filepath} -A hydrogens -o {destination_path}{folder_name}/{receptor_file}qt -U waters', shell=True)
     except:
-        if database_name == 'moad':
-            MOAD_fatal_error_list.append(folder_name)
-            if os.path.isfile('MOAD_fatal_error_list.txt'):
-                os.remove('MOAD_fatal_error_list.txt')
-            with open('MOAD_fatal_error_list.txt','a+') as error_list:
-                error_list.write(str(MOAD_fatal_error_list))
-                error_list.close()
-        if database_name == 'pdb':
-            PDB_fatal_error_list.append(folder_name)
-            if os.path.isfile('PDB_fatal_error_list.txt'):
-                os.remove('PDB_fatal_error_list.txt')
-            with open('PDB_fatal_error_list.txt','a+') as error_list:
-                error_list.write(str(PDB_fatal_error_list))
-                error_list.close()
+        fatal_error_list.append(folder_name)
+        if os.path.isfile('fatal_error_list.txt'):
+            os.remove('fatal_error_list.txt')
+        with open('fatal_error_list.txt','a+') as error_list:
+            error_list.write(str(fatal_error_list))
+            error_list.close()
         print(f'FATAL PROBLEM WITH {folder_name}: Added to list and skipping...')
 
-def batch_convert_to_pdbqt(structure_folders, database_name, destination_path):
-    if database_name == 'moad':
-        destination_path = destination_path + 'Binding_MOAD/'
-        try:
-            os.mkdir(destination_path)
-        except FileExistsError:
-            pass
-        with tqdm(total=len(structure_folders)) as pbar:
-            for folder in structure_folders:
-                folder_name = folder.split('/')
-                folder_name = folder_name[len(folder_name) - 1]
-                files_in_folder = os.listdir(folder)
-                ligand_file = [filename for filename in files_in_folder if 'ligand.pdb' in filename and '.pdbqt' not in filename][0]
-                ligand_filepath = folder + '/' + ligand_file
-                receptor_file = [filename for filename in files_in_folder if 'protein.pdb' in filename and '.pdbqt' not in filename][0]
-                receptor_filepath = folder + '/' + receptor_file
-                autodock_convert(folder_name, destination_path, prep_ligand_command, prep_protein_command, ligand_filepath, ligand_file, receptor_filepath, receptor_file, database_name)
-                pbar.update(1)
+def batch_convert_to_pdbqt(structure_folders, destination_path):
+    try:
+        os.mkdir(destination_path)
+    except FileExistsError:
+        pass
+    with tqdm(total=len(structure_folders)) as pbar:
+        for folder in structure_folders:
+            folder_name = folder.split('/')
+            folder_name = folder_name[len(folder_name) - 1]
+            ligand_file = [filename for filename in os.listdir(folder) if 'ligand.mol2' in filename or 'ligand.pdb' in filename][0]
+            ligand_filepath = folder + '/' + ligand_file
+            receptor_file = [filename for filename in os.listdir(folder) if 'protein.pdb' in filename and '.pdbqt' not in filename][0]
+            receptor_filepath = folder + '/' + receptor_file
+            filetype = ligand_file.split('.')[1]
+            autodock_convert(folder_name, destination_path, prep_ligand_command, prep_protein_command, ligand_filepath, ligand_file, receptor_filepath, receptor_file, filetype)
+            pbar.update(1)
 
-    if database_name == 'pdb':
-        destination_path = destination_path + 'PDBBind/'
-        try:
-            os.mkdir(destination_path)
-        except FileExistsError:
-            pass
-        with tqdm(total=len(structure_folders)) as pbar:
-            for folder in structure_folders:
-                folder_name = folder.split('/')
-                folder_name = folder_name[len(folder_name) - 1]
-                files_in_folder = os.listdir(folder)
-                ligand_file = [filename for filename in files_in_folder if 'ligand.mol2' in filename and '.pdbqt' not in filename][0]
-                ligand_filepath = folder + '/' + ligand_file
-                receptor_file = [filename for filename in files_in_folder if 'protein.pdb' in filename and '.pdbqt' not in filename][0]
-                receptor_filepath = folder + '/' + receptor_file
-                autodock_convert(folder_name, destination_path, prep_ligand_command, prep_protein_command, ligand_filepath, ligand_file, receptor_filepath, receptor_file, database_name)
-                pbar.update(1)
+batch_convert_to_pdbqt(druglike_structure_folders, destination_path)
