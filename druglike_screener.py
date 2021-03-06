dd070bd9aa02e8b1d02e53008cbcc4119f62f996
miles-mcgibbon <s1738716@ed.ac.uk>
2021-03-06 00:18:25 +0000

    Resolved #3: druglike_filter.py now filters problematic ligands before pdbqt conversion

--- druglike_screener.py
+++ druglike_screener.py
@@ -3,11 +3,15 @@ from biopandas.mol2 import PandasMol2
 from molmass import Formula
 import oddt
 
-example_crystal_ligand = '/home/milesm/Dissertation/Data/Parsed/DUD-E/Separated/aa2ar/crystal_ligand.mol2'
-
-def get_mw_from_file(filepath, file_format):
+def get_stats_from_file(filepath, file_format):
     filename = filepath.split('/')
     filename = filename[len(filename) - 1]
     pdb_code = filename.split('.')[0]
-    mol2 = next(oddt.toolkits.ob.readfile(file_format, filepath))
-    return mol2.molwt, mol2.num_rotors
+    mol = next(oddt.toolkits.ob.readfile(file_format, filepath))
+    return mol.molwt, mol.num_rotors, mol.smiles
+
+def druglike_filter(mw, num_rotors, smiles):
+    if mw < 510 and num_rotors <= 13 and len(smiles.split('.') == 1):
+        return True
+    else:
+        return False
