46fc5425a4282e94b3047febff148c79a9d4e605
miles-mcgibbon <s1738716@ed.ac.uk>
2021-03-09 21:27:55 +0000

    Removed unneccesary loop

--- dataCleaning/druglike_screener.py
+++ dataCleaning/druglike_screener.py
@@ -6,19 +6,17 @@ from tqdm import tqdm
 import pandas as pd
 import shutil
 
-pdb_files_path = '/home/milesm/Dissertation/Data/Parsed/Non_redundant/PDBBind/'
-moad_files_path = '/home/milesm/Dissertation/Data/Parsed/Non_redundant/Binding_MOAD/'
+files_path = '/home/milesm/Dissertation/Data/Parsed/Non_redundant/Full/'
 
-pdb_structure_folders = [(pdb_files_path + folder) for folder in os.listdir(pdb_files_path)]
-moad_structure_folders = [(moad_files_path + folder) for folder in os.listdir(moad_files_path)]
-
+structure_folders = [(files_path + folder) for folder in os.listdir(files_path)]
 
 destination_path = '/home/milesm/Dissertation/Data/Parsed/Non_redundant/Full_Druglike/'
 
-def get_stats_from_file(filepath, file_format):
+def get_stats_from_file(filepath):
     filename = filepath.split('/')
     filename = filename[len(filename) - 1]
     pdb_code = filename.split('.')[0]
+    file_format = str(filename.split('.')[1])
     mol = next(oddt.toolkits.ob.readfile(file_format, filepath))
     mw = mol.molwt
     num_rotors = mol.num_rotors
@@ -34,41 +32,18 @@ passes = 0
 
 fails = 0
 
-pass_folders = list()
-
-with tqdm(total=len(moad_structure_folders)) as pbar:
-    for filepath in moad_structure_folders:
-        foldername = filepath.split('/')
-        foldername = foldername[len(foldername) - 1]
-        ligand_file = filepath + '/' + foldername + '_ligand.pdb'
-        ligand_stats = get_stats_from_MOAD_file(ligand_file, 'pdb')
-        if druglike_filter(*ligand_stats):
-            passes = passes + 1
-            pass_folders.append(filepath)
-        else:
-            fails = fails + 1
-        pbar.update(1)
-
-with tqdm(total=len(pdb_structure_folders)) as pbar:
-    for filepath in pdb_structure_folders:
+with tqdm(total=len(structure_folders)) as pbar:
+    for filepath in structure_folders:
         foldername = filepath.split('/')
         foldername = foldername[len(foldername) - 1]
         if foldername == 'index' or foldername == 'readme':
-            fails = fails + 1
+            pass
         else:
-            ligand_file = filepath + '/' + foldername + '_ligand.mol2'
-            ligand_stats = get_stats_from_PDB_file(ligand_file, 'mol2')
+            ligand_file = [f'{filepath}/{filename}' for filename in os.listdir(filepath) if 'ligand' in filename][0]
+            ligand_stats = get_stats_from_file(ligand_file, 'mol2')
             if druglike_filter(*ligand_stats):
                 passes = passes + 1
-                pass_folders.append(filepath)
+                shutil.copytree(filepath, f'{destination_path}{foldername}')
             else:
                 fails = fails + 1
-        pbar.update(1)
-
-with tqdm(total=len(pass_folders)) as pbar:
-    for filepath in pass_folders:
-        foldername = filepath.split('/')
-        foldername = foldername[len(foldername) - 1]
-        ligand_file = [filename for filename in os.listdir(filepath) if 'ligand.mol2' in filename or 'ligand.pdb' in filename][0]
-        shutil.copytree(filepath, f'{destination_path}{foldername}')
         pbar.update(1)
