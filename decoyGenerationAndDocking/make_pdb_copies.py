02c112f3b535de2721b5d07922eefbf1615e6a51
miles-mcgibbon <s1738716@ed.ac.uk>
2021-03-19 23:29:58 +0000

    Added script to make .pdb copy of working .pdbqt files

---
+++ decoyGenerationAndDocking/make_pdb_copies.py
@@ -0,0 +1,39 @@
+import os
+import oddt
+import subprocess
+from tqdm import tqdm
+import shutil
+import sys
+
+def copy_pdb_files(original_pdb_files_path, destination_path, pdbqt_path_folders):
+    try:
+        os.mkdir(destination_path)
+    except FileExistsError:
+        pass
+    with tqdm(total=len(pdbqt_path_folders)) as pbar:
+        for folder in pdbqt_path_folders:
+            folder_name = folder.split('/')
+            folder_name = folder_name[len(folder_name) - 1]
+            shutil.copytree(f'{original_pdb_files_path}{folder_name}', f'{destination_path}{folder_name}')
+            pbar.update(1)
+
+def parse_args(args):
+
+    original_pdb_files_path = args[args.index('-loc') + 1]
+
+    destination_path = args[args.index('-des') + 1]
+
+    pdbqt_path = args[args.index('-ref') + 1]
+
+    return original_pdb_files_path, destination_path, pdbqt_path
+
+def main():
+
+    original_pdb_files_path, destination_path, pdbqt_path = parse_args(sys.argv)
+    
+    pdbqt_path_folders = [(pdbqt_path + folder) for folder in os.listdir(pdbqt_path)]
+
+    copy_pdb_files(original_pdb_files_path, destination_path, pdbqt_path_folders)
+
+if __name__ == '__main__':
+    main()
