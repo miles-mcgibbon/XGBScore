b5f42db14fdb6acf1d21e2ceac659fc6fde82bb4
miles-mcgibbon <s1738716@ed.ac.uk>
2021-03-31 02:03:02 +0100

    Updated comments pdbqt_batch_converter.py

--- dataCleaning/pdbqt_batch_converter.py
+++ dataCleaning/pdbqt_batch_converter.py
@@ -7,20 +7,27 @@ import sys
 
 fatal_error_list = list()
 
-def autodock_convert(folder_name, destination_path, prep_ligand_command, prep_protein_command, ligand_filepath, ligand_file, receptor_filepath, receptor_file, filetype):
+def autodock_convert(folder_name, destination_path, prep_ligand_command, prep_protein_command, ligand_filepath, ligand_file, receptor_filepath, receptor_file, filetype): # converts pdb files to pdbqt
+
+    # prepare destination folder
     try:
         os.mkdir(f'{destination_path}{folder_name}')
         print(f'Made directory {destination_path}{folder_name}')
     except FileExistsError:
         pass
     print(folder_name)
+
+    # get filetype and alter ligand outfile
     if filetype == 'mol2':
         ligand_outfile = ligand_file.replace('mol2','pdb')
     else:
         ligand_outfile = ligand_file
+
+    # convert ligand to pdbqt using AutoDockTools adding polar hydrogens and gasteiger charges
     print('Preparing ligand...')
     try:
         output = subprocess.check_output(f'{prep_ligand_command} -l {ligand_filepath} -A hydrogens -o {destination_path}{folder_name}/{ligand_outfile}qt -U nphs', shell=True, stderr=subprocess.STDOUT)
+    # add problematic ligands to fatal_error_list
     except:
         fatal_error_list.append(folder_name)
         if os.path.isfile('fatal_error_list.txt'):
@@ -30,9 +37,12 @@ def autodock_convert(folder_name, destination_path, prep_ligand_command, prep_pr
             error_list.close()
         print(f'FATAL PROBLEM WITH {folder_name}: Added to list and skipping...')
 
+    # convert receptor to pdbqt using AutoDockTools adding polar hydrogens and gasteiger charges
     print('Preparing protein...')
     try:
         output = subprocess.check_output(f'{prep_protein_command} -r {receptor_filepath} -A hydrogens -o {destination_path}{folder_name}/{receptor_file}qt -U nphs', shell=True)
+
+    # add problematic ligands to fatal_error_list
     except:
         fatal_error_list.append(folder_name)
         if os.path.isfile('fatal_error_list.txt'):
@@ -42,13 +52,19 @@ def autodock_convert(folder_name, destination_path, prep_ligand_command, prep_pr
             error_list.close()
         print(f'FATAL PROBLEM WITH {folder_name}: Added to list and skipping...')
 
-def batch_convert_to_pdbqt(structure_folders, destination_path, prep_ligand_command, prep_protein_command):
+def batch_convert_to_pdbqt(structure_folders, destination_path, prep_ligand_command, prep_protein_command): # wrapper to batch convert ligands from a list of structure folders
+
+    # make destination folder
     try:
         os.mkdir(destination_path)
     except FileExistsError:
         pass
+
+    # loop over structure folders
     with tqdm(total=len(structure_folders)) as pbar:
         for folder in structure_folders:
+
+            # get variables from filename
             print(folder)
             folder_name = folder.split('/')
             folder_name = folder_name[len(folder_name) - 1]
@@ -57,10 +73,12 @@ def batch_convert_to_pdbqt(structure_folders, destination_path, prep_ligand_comm
             receptor_file = [filename for filename in os.listdir(folder) if 'receptor.pdb' in filename or 'pocket.pdb' in filename][0]
             receptor_filepath = folder + '/' + receptor_file
             filetype = ligand_file.split('.')[1]
+
+            # convert to pdbqt
             autodock_convert(folder_name, destination_path, prep_ligand_command, prep_protein_command, ligand_filepath, ligand_file, receptor_filepath, receptor_file, filetype)
             pbar.update(1)
 
-def move_problematic_structures(fatal_error_list, druglike_files_path, destination_path, problem_path):
+def move_problematic_structures(fatal_error_list, druglike_files_path, destination_path, problem_path): # copy structures in fatal_error_list to location for manual inspection
     fatal_error_list = list(set(list(fatal_error_list)))
     with tqdm(total=len(fatal_error_list)) as pbar:
         for folder in fatal_error_list:
@@ -68,7 +86,7 @@ def move_problematic_structures(fatal_error_list, druglike_files_path, destinati
             shutil.copytree(f'{druglike_files_path}{folder}', f'{problem_path}{folder}')
             pbar.update(1)
 
-def parse_args(args):
+def parse_args(args): # parse CLI user inputs
 
     druglike_files_path = args[args.index('-loc') + 1]
 
@@ -82,7 +100,7 @@ def parse_args(args):
 
     return druglike_files_path, destination_path, problem_path, prep_ligand_command, prep_protein_command
 
-def main():
+def main(): # run script using CLI
 
     druglike_files_path, destination_path, problem_path, prep_ligand_command, prep_protein_command = parse_args(sys.argv)
 
