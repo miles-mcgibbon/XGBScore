417545fa1b28d37b68b79c63cc936dabc066d4b2
miles-mcgibbon <s1738716@ed.ac.uk>
2021-03-31 02:16:40 +0100

    Updated comments make_pdb_copies.py

--- decoyGenerationAndDocking/make_pdb_copies.py
+++ decoyGenerationAndDocking/make_pdb_copies.py
@@ -5,11 +5,15 @@ from tqdm import tqdm
 import shutil
 import sys
 
-def copy_pdb_files(original_pdb_files_path, destination_path, pdbqt_path_folders):
+def copy_pdb_files(original_pdb_files_path, destination_path, pdbqt_path_folders): # simple function to copy pdb files to destination from source
+
+    # prepare destination folder
     try:
         os.mkdir(destination_path)
     except FileExistsError:
         pass
+
+    # copy folder
     with tqdm(total=len(pdbqt_path_folders)) as pbar:
         for folder in pdbqt_path_folders:
             folder_name = folder.split('/')
@@ -17,7 +21,7 @@ def copy_pdb_files(original_pdb_files_path, destination_path, pdbqt_path_folders
             shutil.copytree(f'{original_pdb_files_path}{folder_name}', f'{destination_path}{folder_name}')
             pbar.update(1)
 
-def parse_args(args):
+def parse_args(args): # parse CLI user inputs
 
     original_pdb_files_path = args[args.index('-loc') + 1]
 
@@ -27,10 +31,10 @@ def parse_args(args):
 
     return original_pdb_files_path, destination_path, pdbqt_path
 
-def main():
+def main(): # run script using CLI
 
     original_pdb_files_path, destination_path, pdbqt_path = parse_args(sys.argv)
-    
+
     pdbqt_path_folders = [(pdbqt_path + folder) for folder in os.listdir(pdbqt_path)]
 
     copy_pdb_files(original_pdb_files_path, destination_path, pdbqt_path_folders)
