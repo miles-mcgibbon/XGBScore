8429d466f36f1a5f6ad3fed81d03b1d6512d0662
miles-mcgibbon <s1738716@ed.ac.uk>
2021-03-31 01:16:24 +0100

    Updated comments MOAD_poccked_and_ligand_isolator.py

--- dataCleaning/MOAD_pocket_and_ligand_isolator.py
+++ dataCleaning/MOAD_pocket_and_ligand_isolator.py
@@ -1,8 +1,3 @@
-# this script should load up a full pdb or biounit file into biopython.
-# Then get the coordinates of the ligand atoms, and define a box around the ligand
-# Then check which atoms are inside the box by their coordinates, and get a list of the residues these atoms belong to
-# Then it should save all the residues that are in that list in a receptor file, and save the ligand in a ligand file
-
 import os
 from Bio.PDB import *
 from tqdm import tqdm
@@ -13,44 +8,45 @@ from warnings import filterwarnings
 from tqdm import tqdm
 from itertools import chain as iterchain
 
-# ignore warnings about incomplete PDB structures, as Binding MOAD supplies truncated biounits
-filterwarnings('ignore')
-
-class ligand_selector(Select):
+class ligand_selector(Select): # selector class for BioPython which saves all residues in supplied 'keep_ligand_residues' list
 
     def __init__(self, keep_ligand_residues):
         self.keep_ligand_residues = keep_ligand_residues
 
-    def accept_residue(self, residue):
-        # select residues from identified ligand
+    def accept_residue(self, residue): # accept residues if unique identifier present in 'keep_ligand_residues' list
         return True if (str(residue.id[1]) + residue.get_parent().id) in self.keep_ligand_residues else False
 
-class pocket_selector(Select):
+class pocket_selector(Select): # selector class for BioPython which saves all residues in supplied 'pocket_residues' list
 
     def __init__(self, pocket_residues):
         self.pocket_residues = pocket_residues
 
-    def accept_residue(self, residue):
-        # select residues from identified ligand
+    def accept_residue(self, residue): # accept residues if unique identifier present in 'pocket_residues' list
         return True if (str(residue.id[1]) + residue.get_parent().id) in self.pocket_residues else False
 
-def define_pocket(structure, chain_residue, boundary):
+def define_pocket(structure, chain_residue, cutoff_thresh): # calculate cuboid coordinates around ligand with padding of user defined 'cutoff_thresh' angstroms
+
+    # construct list of coordinates of all atoms in ligand
     atom_coord_data = list()
     for atom in chain_residue.get_atoms():
         atom_coord_data.append(atom.get_coord())
+
+    # turn coordinates into dataframe
     atom_df = pd.DataFrame(atom_coord_data, columns=['X','Y','Z'])
-    min_x, max_x = (min(atom_df.X) - boundary), (max(atom_df.X) + boundary)
-    min_y, max_y = (min(atom_df.Y) - boundary), (max(atom_df.Y) + boundary)
-    min_z, max_z = (min(atom_df.Z) - boundary), (max(atom_df.Z) + boundary)
+
+    # calculate min and max of x, y, z coordinates and pad with 'cutoff_thresh' angstroms
+    min_x, max_x = (min(atom_df.X) - cutoff_thresh), (max(atom_df.X) + cutoff_thresh)
+    min_y, max_y = (min(atom_df.Y) - cutoff_thresh), (max(atom_df.Y) + cutoff_thresh)
+    min_z, max_z = (min(atom_df.Z) - cutoff_thresh), (max(atom_df.Z) + cutoff_thresh)
     return (min_x, min_y, min_z, max_x, max_y, max_z)
 
-def pocket_check(x, y, z, min_x, min_y, min_z, max_x, max_y, max_z):
+def pocket_check(x, y, z, min_x, min_y, min_z, max_x, max_y, max_z): # check if an atom is within the cuboid produced by 'define_pocket' function
     if min_x < x < max_x and min_y < y < max_y and min_z < z < max_z:
         return True
     else:
         return False
 
-def get_ligand_information(structure, valid_ligand_ids, valid_chain_id):
+def get_ligand_information(structure, valid_ligand_ids, valid_chain_id):  # construct dataframe of ligand residues id, number, insertion code, chain letter
     ligand_residue_data = list()
     chain_data = list()
     for residue in structure.get_residues():
@@ -62,11 +58,15 @@ def get_ligand_information(structure, valid_ligand_ids, valid_chain_id):
     ligand_df['CHAIN'] = chain_data
     return ligand_df.loc[ligand_df.CHAIN == valid_chain_id]
 
-def broken_ligand_check(filepath, file_format, valid_ligand_ids, valid_chain_id, structure):
+def broken_ligand_check(filepath, file_format, valid_ligand_ids, valid_chain_id, structure): # test for separate ligands stored as one chain
+
+    # pass test if only single residue in ligand
     ligand_information = get_ligand_information(structure, valid_ligand_ids, valid_chain_id)
     if len(ligand_information) == 1:
         return True
     else:
+
+        # iterate through atoms and bonds to populate bonds_dictionary
         mol = next(oddt.toolkits.ob.readfile(file_format, filepath))
         bonds_dictionary = dict()
         residue_identifiers = list()
@@ -80,21 +80,26 @@ def broken_ligand_check(filepath, file_format, valid_ligand_ids, valid_chain_id,
                         for atom in bond.atoms:
                             atoms_in_residue.append(atom.idx)
                 bonds_dictionary[residue_identifier] = atoms_in_residue
+
+        # check for continuous shared bonds between residues
         shared_bonds = None
         for index, item in enumerate(residue_identifiers):
             try:
                 shared_bonds = len(list(set(bonds_dictionary[residue_identifiers[index]]).intersection(bonds_dictionary[residue_identifiers[index + 1]])))
             except IndexError:
                 pass
+
+        # fail the test if no shared bonds found between residues else pass
         if shared_bonds == 0:
             return False
         else:
             return True
 
-def isolate_pocket_and_ligand(filename, success_destination_path, problem_destination_path, datafile, cutoff_thresh, exclusion_thresh):
+def isolate_pocket_and_ligand(filename, success_destination_path, problem_destination_path, datafile, cutoff_thresh, exclusion_thresh): # save separate receptor and ligand pdb files
 
     amino_acids = ['ALA', 'ARG', 'ASN', 'ASP', 'ASX', 'CYS', 'GLU', 'GLN', 'GLX', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL']
 
+    # default keep structure unless problem found
     keep_structure = True
 
     # define pdb code
@@ -108,9 +113,12 @@ def isolate_pocket_and_ligand(filename, success_destination_path, problem_destin
     # get the id of the active ligand
     pdb_code_data = datafile.loc[datafile.PDBCode.str.upper() == pdb_code.upper()]
     ligand_data = pdb_code_data.loc[pdb_code_data.Validity == 'valid']
+
+    # get the chain id of one active ligand if multiple present
     valid_ligand_chain = [ligand.split(':')[1] for ligand in list(ligand_data.LigandID)][0]
     valid_ligand_ids = [ligand.split(':')[0].split(' ') for ligand in list(ligand_data.LigandID)]
 
+    # discard structure if amino acids present in ligand
     if len(list(set(valid_ligand_ids[0]).intersection(amino_acids))) != 0:
         print('Peptide Ligand detected!')
         keep_structure = False
@@ -129,6 +137,7 @@ def isolate_pocket_and_ligand(filename, success_destination_path, problem_destin
     io = PDBIO()
     io.set_structure(structure)
 
+    # check ligand for residues with no shared bonds
     if not broken_ligand_check(filename, 'pdb', valid_ligand_ids, valid_ligand_chain, structure):
         keep_structure = False
         print('BROKEN LIGAND')
@@ -136,61 +145,76 @@ def isolate_pocket_and_ligand(filename, success_destination_path, problem_destin
 
     else:
 
+        # set up lists and dicts for populating
         ligand_dimensions = dict()
-
         exclusion_dimensions = dict()
-
         pocket_residues = list()
-
         keep_ligand_residues = list()
-
-        ligand_count = 1
 
         model = structure[0]
         chain = model[valid_ligand_chain]
 
+        # loop through residues in chosen active ligand chain
         for chain_residue in chain.get_residues():
             if chain_residue.id[0].replace('H_','') in valid_ligand_ids[0]:
+
+                # if residue is in valid ligands make unique ID
                 residue_unique_id = str(chain_residue.id[1]) + chain_residue.get_parent().id
+
+                # calculate user defined receptor cuboid around the ligand
                 ligand_pocket_dimensions = define_pocket(structure, chain_residue, cutoff_thresh)
-                ligand_exclusion = define_pocket(structure, chain_residue, exclusion_thresh)
                 ligand_dimensions[chain_residue.id[0].replace('H_','')] = ligand_pocket_dimensions
+
+                # calculate user defined exclusion cuboid around the ligand
+                ligand_exclusion = define_pocket(structure, chain_residue, exclusion_thresh)
                 exclusion_dimensions[chain_residue.id[0].replace('H_','')] = ligand_exclusion
+
+                # add ligand residues to list for ligand selector class
                 keep_ligand_residues.append(residue_unique_id)
 
+        # second check for peptide ligands stored as single letter amino acid codes
         if len(ligand_dimensions) == 0:
-            # only true if no ligand contains no HETATMS - second check for peptide ligands stored as single letter amino acid codes
             print('Peptide Ligand detected!')
             keep_structure = False
 
+        # loop through protein residues
         for residue in structure.get_residues():
             for atom in residue.get_atoms():
                 x, y, z = atom.get_coord()
+                # check if any hetatms from a different chain/ligand are within the exclusion zone
                 for exclusion in exclusion_dimensions.values():
                     if pocket_check(x, y, z, *exclusion):
                         if 'H' in atom.get_parent().id[0]:
                             if atom.get_parent().id[0].replace('H_','') in valid_ligand_ids[0]:
+                                # ignore if hetatms are from the ligand itself
                                 if str(atom.get_parent().get_parent().id) == valid_ligand_chain:
                                     pass
+                                # ignore if hetatms are from bound metal ions or cofactors listed as part of protein
                                 elif atom.get_parent().id[0].replace('H_','') in protein_part_ids:
                                     pass
+                                # discard structure if hetatms found in exclusion zone that are not either of the above exceptions
                                 else:
                                     keep_structure = False
 
-
+                # find and record any protein residues with atoms inside the receptor cutoff
                 for dimension in ligand_dimensions.values():
                     if pocket_check(x, y, z, *dimension):
                         residue_unique_id = str(atom.get_parent().id[1]) + atom.get_parent().get_parent().id
+                        # ignore and remove water molecules
                         if atom.get_parent().id[0] == 'W':
                             pass
                         elif 'H' in atom.get_parent().id[0]:
+                            # ignore ligand atoms
                             if atom.get_parent().id[0].replace('H_','').strip().lstrip() in valid_ligand_ids[0]:
                                 pass
+                            # save hetatms if listed as part of protein
                             elif atom.get_parent().id[0].replace('H_','').strip().lstrip() in protein_part_ids:
                                 pocket_residues.append(residue_unique_id)
+                        # save amino acid residues
                         elif atom.get_parent().id[0] == ' ':
                             pocket_residues.append(residue_unique_id)
 
+    # save ligand and receptor pdb files if no problems found
     if keep_structure:
         try:
             os.mkdir(f'{success_destination_path}{pdb_code}')
@@ -200,6 +224,8 @@ def isolate_pocket_and_ligand(filename, success_destination_path, problem_destin
         io.save(f'{success_destination_path}{pdb_code}/{pdb_code}_ligand.pdb', ligand_selector(keep_ligand_residues))
         io.save(f'{success_destination_path}{pdb_code}/{pdb_code}.pdb')
         print('Saved files!')
+
+    # otherwise make copy of original pdb structure for manual verification
     else:
         try:
             os.mkdir(f'{problem_destination_path}{pdb_code}')
@@ -209,7 +235,7 @@ def isolate_pocket_and_ligand(filename, success_destination_path, problem_destin
         io.save(f'{problem_destination_path}{pdb_code}/{pdb_code}.pdb')
         pass
 
-def parse_args(args):
+def parse_args(args): # parse CLI user inputs
 
     structure_location = args[args.index('-loc') + 1]
 
@@ -225,7 +251,7 @@ def parse_args(args):
 
     return structure_location, success_destination_path, problem_destination_path, binding_MOAD_datafile_path, cutoff_thresh, exclusion_thresh
 
-def main():
+def main(): # run script using CLI
 
     structure_location, success_destination_path, problem_destination_path, binding_MOAD_datafile_path, cutoff_thresh, exclusion_thresh = parse_args(sys.argv)
 
@@ -233,6 +259,7 @@ def main():
 
     structures = [(structure_location + folder) for folder in structure_folders]
 
+    # load binding MOAD reference dataframe with forced headers and clean up formatting
     force_columns = ['Decimal',
                  'EntryString',
                  'PDBCode',
@@ -244,11 +271,10 @@ def main():
                  'BindingUnits',
                  'FormulaString',
                  'FormulaStringSpill']
-
     binding_df = pd.read_csv(binding_MOAD_datafile_path, names=force_columns)
-
     binding_df['PDBCode'] = binding_df['PDBCode'].ffill()
 
+    # loop through structures and isolate receptor and ligand files
     with tqdm(total=len(structures)) as pbar:
         for structure_file in structures:
             isolate_pocket_and_ligand(structure_file, success_destination_path, problem_destination_path, binding_df, cutoff_thresh, exclusion_thresh)
